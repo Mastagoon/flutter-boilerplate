78680ce028f4d1fdaff5dec10ad64401c5ae0cbb
Mastagoon <inzilagiway@gmail.com>
2021-07-30 19:49:11 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      title: "Test",
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Container(
+          child: Text("Hello World"),
+        ),
+      ),
+    ));
